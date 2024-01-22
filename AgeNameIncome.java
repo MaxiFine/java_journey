1cbdc94031122b072604c94de199afbf11249494
Maxwell Adomako <maxwell.adomako@amalitech.com>
2024-01-22 18:50:47 +0000

    inputs trial 1

---
+++ AgeNameIncome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AgeNameIncome {
+
+    public static void  main(String[] args){
+
+        String name;
+        int age;
+        double income;
+
+        // set the input scanner
+        Scanner key = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        name = key.nextLine();
+
+        System.out.println("Enter age: ");
+        age = key.nextInt();
+
+        System.out.println("Ener you Income: ");
+        income = key.nextDouble();
+
+        System.out.println("My name is " + name + ", my age is " + age + "\n" +
+                "I hope to earn $" + income + " per year.");
+
+
+
+
+    }
+}
